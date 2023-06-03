7b95cc9f5d90921a54b41971262974802a7a6492
vimanshani <pabodhavimanshani@gmail.com>
2023-06-03 06:37:09 -0700

    3

---
+++ cal.js
@@ -0,0 +1,32 @@
+function caladd(){
+
+      let fn= parseFloat(document.getElementById("firstnumber").value) ;
+      let sn= parseFloat(document.getElementById("secondnumber" ).value);
+  
+      document.getElementById("resultnumber").value= fn+sn;
+ 
+ }
+ function calsub(){
+    
+      let fn= parseFloat(document.getElementById("firstnumber").value) ;
+      let sn= parseFloat(document.getElementById("secondnumber" ).value);
+  
+      document.getElementById("resultnumber").value= fn-sn;
+
+ }
+ function calmulti(){
+    
+      let fn= parseFloat(document.getElementById("firstnumber").value) ;
+      let sn= parseFloat(document.getElementById("secondnumber" ).value);
+  
+      document.getElementById("resultnumber").value= fn*sn;
+ 
+ }
+ function caldiv(){
+
+      let fn= parseFloat(document.getElementById("firstnumber").value) ;
+      let sn= parseFloat(document.getElementById("secondnumber" ).value);
+  
+      document.getElementById("resultnumber").value= fn/sn;
+ 
+ }
